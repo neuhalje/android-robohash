d00fbb11f5efca6d53132b0ef094b267f2d15720
Jens Neuhalfen <jens@neuhalfen.name>
2015-09-24 19:07:12 +0200

    Fix unit test: image numbers are 1-based, bucket elements 0-based

--- src/test/groovy/name/neuhalfen/projects/android/robohash/paths/Set1ConfigurationTest.groovy
+++ src/test/groovy/name/neuhalfen/projects/android/robohash/paths/Set1ConfigurationTest.groovy
@@ -15,15 +15,15 @@ class Set1ConfigurationTest extends spock.lang.Specification {
 
         where:
         bucketValues       || expectedPaths
-        [0, 1, 2, 3, 4, 5] || ["sets/set1/blue/01Body/blue_body-01.png",
-                               "sets/set1/blue/02Face/blue_face-02.png",
-                               "sets/set1/blue/Mouth/blue_mouth-03.png",
-                               "sets/set1/blue/Eyes/blue_eyes-04.png",
-                               "sets/set1/blue/Accessory/blue_accessory-05.png",]
-        [0, 1, 2, 3, 4, 10] || ["sets/set1/blue/01Body/blue_body-01.png",
-                               "sets/set1/blue/02Face/blue_face-02.png",
-                               "sets/set1/blue/Mouth/blue_mouth-03.png",
-                               "sets/set1/blue/Eyes/blue_eyes-04.png",
-                               "sets/set1/blue/Accessory/blue_accessory-10.png",]
+        [0, 1, 2, 3, 4, 5] || ["sets/set1/blue/01Body/blue_body-02.png",
+                               "sets/set1/blue/02Face/blue_face-03.png",
+                               "sets/set1/blue/Mouth/blue_mouth-04.png",
+                               "sets/set1/blue/Eyes/blue_eyes-05.png",
+                               "sets/set1/blue/Accessory/blue_accessory-06.png"]
+        [1, 0, 1, 2, 3, 9] || ["sets/set1/brown/01Body/brown_body-01.png",
+                               "sets/set1/brown/02Face/brown_face-02.png",
+                               "sets/set1/brown/Mouth/brown_mouth-03.png",
+                               "sets/set1/brown/Eyes/brown_eyes-04.png",
+                               "sets/set1/brown/Accessory/brown_accessory-10.png"]
     }
 }
